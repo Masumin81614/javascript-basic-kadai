207db7c3e4de5d88ed738f0af2a7561c3eecb9e0
Masumin81614 <hlkcorporation@gmail.com>
2024-08-11 18:53:58 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,13 +1,7 @@
-const date = new Date("2000-01-17T16:45:30");
-const [month, day, year] = [
-  date.getMonth(),
-  date.getDate(),
+const date = new Date();
+const [year, month, day ] = [
   date.getFullYear(),
+  date.getMonth()+ 1,
+  date.getDate(),
 ];
-// [0, 17, 2000] （月は 0 基点であるため）
-const [hour, minutes, seconds] = [
-  date.getHours(),
-  date.getMinutes(),
-  date.getSeconds(),
-];
-console.log(date);
+console.log(year + '年' + month +'月' + day + '日');
